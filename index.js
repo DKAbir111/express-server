1f1810a69ef00f03bb4a8c618c5902894e80cbf9
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-11-22 19:34:03 +0600

    create api

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 5001
+const express = require('express');
+const coupons = require('./data.json')
+const app = express();
+const port = 5001;
+
 app.get('/', (req, res) => {
-    res.send("Hello this is a test server")
+    res.send("Welcome to my API server!")
 })
 
+app.get('/coupons', (req, res) => {
+    res.send(coupons);
+})
+
+app.get('/coupon/:id', (req, res) => {
+    const id = req.params.id
+    res.send(coupons.find(coupon => coupon._id === id))
+})
 app.listen(port, () => {
-    console.log(`App is runniing on port ${port}`)
+    console.log(`Server is running at http://localhost:${port}`)
 })
